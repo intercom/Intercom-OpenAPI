5b2a4cc86aefbcda4a241b1153c41d2ccb70e93c
Suma Prakash <suma prakash@intercom.io>
2023-02-02 12:46:57 +0000

    Adding action

--- scripts/run-sync.js
+++ scripts/run-sync.js
@@ -3,7 +3,7 @@ const uploadAPISpecification = require('./upload-api-specification');
 
 (async () => {
   //fetch the changed files
-  const files = ['/descriptions/5.0/api.intercom.io.yaml'];
+  const files = process.argv.slice(2);
   for (const file of files) {
     const filepath = path.join(__dirname, '..', file);
     await uploadAPISpecification(filepath);
