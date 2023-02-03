d87cd7e79cab2b66e46b159d041af1c7daca0570
Suma Prakash <suma prakash@intercom.io>
2023-02-03 10:34:46 +0000

    test

--- scripts/run-sync.js
+++ scripts/run-sync.js
@@ -1,11 +1,19 @@
 const path = require('path');
 const uploadAPISpecification = require('./upload-api-specification');
+const core = require('@actions/core');
+const github = require('@actions/github');
 
 (async () => {
   //fetch the changed files
-  const files = process.argv.slice(2,-1);
-  for (const file of files) {
-    const filepath = path.join(__dirname, '..', file);
-    await uploadAPISpecification(filepath);
+  try{
+    const files = process.argv.slice(2,-1);
+    for (const file of files) {
+      const filepath = path.join(__dirname, '..', file);
+      await uploadAPISpecification(filepath);
+    }
   }
+  catch(err){
+    core.setFailed(err.message);
+  }
+
 })();
